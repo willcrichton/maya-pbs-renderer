fce679c7f85c9f93ad4b7a90c239a45df9131a00
Will Crichton <crichton.will@gmail.com>
2016-04-20 15:34:37 -0400

    Better command line arguments

--- render.py
+++ render.py
@@ -1,46 +1,62 @@
-#!python
-import argparse, time, tempfile, subprocess
+#!/opt/python/bin/python
+import argparse, time, tempfile, subprocess, os, shutil
 
 def die(err):
     print "Error: {}".format(err)
     exit(1)
 
+MAX_TIME_PER_JOB = 60 * 20 # 20 min in default queue
+
 def main():
-    parser = argparse.ArgumentParser(description='Batch render a Maya scene.', epilog='Example usage: ./render.py -s -5 -e 100 -a "-cam my_camera" ~/final_proj/scenes/final.mb')
-    parser.add_argument('-s', '--start', help='Starting frame to render', required=True, type=int)
-    parser.add_argument('-e', '--end', help='Ending frame to render', required=True, type=int)
-    parser.add_argument('-j', '--jobs', help='Number of jobs to use for rendering', type=int, default=4)
-    parser.add_argument('-t', '--time_per_frame', help='Reasonable upper bound on render time per frame', type=int, default=30)
-    parser.add_argument('-a', '--args', help='Additional arguments to pass to the Maya renderer.', default='')
-    parser.add_argument('scene_path', help='Path of scene to render')
+    parser = argparse.ArgumentParser(
+        description='Batch render a Maya scene.',
+        epilog='Example usage: ./render.py -s -5 -e 100 -a "-cam my_camera" /home/wcrichto/final_proj scenes/final.mb')
+    parser.add_argument('project', help='Absolute path of Maya project folder')
+    parser.add_argument('scene', help='Path of scene to render relative to project')
+    parser.add_argument('start', help='Starting frame to render', type=int)
+    parser.add_argument('end', help='Ending frame to render', type=int)
+    parser.add_argument('-t', '--time_per_frame', type=int, default=1,
+                        help='Upper bound on render time per frame in minutes')
+    parser.add_argument('-a', '--args', default='',
+                        help='Additional arguments to pass to the Maya renderer.')
+    parser.add_argument('-o', '--output', default='/home/wcrichto/maya-output',
+                        help='Directory to output rendered frames to')
     args = parser.parse_args()
 
-    if args.start > args.end: die("Start frame must be less than or equal to end frame")
-    if args.jobs <= 0: die("Must have more than zero jobs")
-    
+    if args.start > args.end:
+        die("Start frame must be less than or equal to end frame")
+
     total_frames = args.end - args.start + 1
-    frames_per_job = total_frames / args.jobs
-    extra_frames = total_frames % args.jobs
-    job_walltime = time.strftime('%H:%M:%S', time.gmtime(frames_per_job * args.time_per_frame))
+    total_time = total_frames * args.time_per_frame * 60
+    jobs = max(total_time / MAX_TIME_PER_JOB, 1)
+    frames_per_job = total_frames / jobs
+    extra_frames = total_frames % jobs
+    job_walltime = time.strftime('%H:%M:%S', time.gmtime(MAX_TIME_PER_JOB))
+
+    # print "Frames %d, jobs %d, frames per job: %d" % (total_frames, jobs, frames_per_job)
 
     with open('template.job', 'r') as f: job_template = f.read()
+    shutil.rmtree('jobs')
+    os.makedirs('jobs')
 
-    for i in xrange(args.jobs):
+    for i in xrange(jobs):
         job_start = i * frames_per_job + args.start
         job_end = job_start + frames_per_job - 1
-        if i == args.jobs - 1: job_end += extra_frames
+        if i == jobs - 1: job_end += extra_frames
         format_args = {
             'walltime': job_walltime,
             'start': job_start,
             'end': job_end,
             'index': i,
             'args': args.args,
-            'file': args.scene_path
+            'project': args.project,
+            'scene': args.scene,
+            'output': args.output
             }
-        with tempfile.NamedTemporaryFile() as job_file:
+        with open('jobs/maya_render_{}.job'.format(i), 'wb') as job_file:
             job_file.write(job_template.format(**format_args))
-            subprocess.check_call("qsub {}".format(job_file.name), shell=True)
-        
+            #subprocess.check_call('qsub {}'.format(job_file.name), shell=True)
+
 
 if __name__ == "__main__":
     main()
---
+++ render.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+./render.py $@
+RESULT=$?
+if (! [[ $* == *-h* ]]) && [[ $RESULT -eq 0 ]];
+then
+    for f in jobs/*; do
+	qsub $f
+    done
+fi
